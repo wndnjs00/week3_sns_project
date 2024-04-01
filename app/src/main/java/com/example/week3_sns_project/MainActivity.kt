9be1cc69bae14bff713d242892dadbb5245b3e52
jeonjuwon <juwonjeon852@gmail.com>
2024-04-01 22:06:25 +0900

    [DESIGN] main페이지와 home페이지 xml 디자인

--- app/src/main/java/com/example/week3_sns_project/MainActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.week3_sns_project
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +10,50 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+
+        BottomNavigation()
     }
+
+
+
+    // bottomNavigation 구현함수
+    private fun BottomNavigation(){
+
+
+        val bottonm_nav = findViewById<BottomNavigationView>(R.id.main_bottomnavgation)
+
+
+        // main_framelayout레이아웃을 HomeFragment로 대체
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_framelayout, HomeFragment())
+            .commitAllowingStateLoss()
+
+
+        bottonm_nav.setOnItemSelectedListener { item ->
+
+            when (item.itemId){
+
+                // home클릭시 HomeFragment()로 대체
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_framelayout, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                // mypage클릭시 MypageFragment()로 대체
+//                R.id.mypage -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_framelayout, MypageFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+//                }
+
+            }
+            false
+        }
+
+
+    }
+
 }
