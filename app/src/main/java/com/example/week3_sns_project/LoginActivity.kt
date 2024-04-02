70e480679653f34a3fc7d056137c99e7b02b5154
AndroidJeong <okbamtol@gmail.com>
2024-04-02 16:25:35 +0900

    로그인&회원가입 페이지 추가

---
+++ app/src/main/java/com/example/week3_sns_project/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.week3_sns_project
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+
+        val loginBackButton: ImageButton = findViewById(R.id.login_back_button)
+
+        loginBackButton.setOnClickListener{
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
