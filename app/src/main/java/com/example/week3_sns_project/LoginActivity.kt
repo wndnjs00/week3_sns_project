7ea93a0f64d73ec4633f28064ec8bfaba0bc0797
AndroidJeong <okbamtol@gmail.com>
2024-04-02 11:13:40 +0900

    초기화면 & 로그인 & 회원가입 페이지

---
+++ app/src/main/java/com/example/week3_sns_project/LoginActivity.kt
@@ -0,0 +1,34 @@
+package com.example.week3_sns_project
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        val loginBackButton: ImageButton = findViewById(R.id.login_back_button)
+
+        loginBackButton.setOnClickListener{
+            finish()
+        }
+
+      /*
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+       */
+    }
+}
--- app/src/main/java/com/example/week3_sns_project/MainActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.week3_sns_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val loginButton: Button = findViewById(R.id.main_login_button)
+        val signUpButton: Button = findViewById(R.id.main_signup_button)
+
+        val loginIntent = Intent(this@MainActivity, LoginActivity::class.java)
+        val signUpIntent = Intent(this@MainActivity, SignUpActivity::class.java)
+
+        loginButton.setOnClickListener{
+            startActivity(loginIntent)
+        }
+
+        signUpButton.setOnClickListener{
+            startActivity(signUpIntent)
+        }
 
     }
 }
