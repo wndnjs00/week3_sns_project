38a7e73e679af809ed175ed69473b14ff435ce31
jeonjuwon <juwonjeon852@gmail.com>
2024-04-02 22:39:02 +0900

    [FIX]디렉토리 파일구조 수정

--- app/src/main/java/com/example/week3_sns_project/HomeFragment.kt
+++ app/src/main/java/com/example/week3_sns_project/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.example.week3_sns_project.Home
+package com.example.week3_sns_project
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,8 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.week3_sns_project.DetailActivity
-import com.example.week3_sns_project.R
 import com.example.week3_sns_project.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -26,7 +24,6 @@ class HomeFragment : Fragment() {
 
         itemList.add(DataModel(R.drawable.img_bts, "방탄 카페 용산점에 왔어요", "게시글 내용 어쩌구"))
         itemList.add(DataModel(R.drawable.img_btob, "비투비 콘서트 갔다왔어요!!","게시글 내용 어쩌구2"))
-
 
         val adapter = HomeAdapter(itemList)
         binding.homeRecyclerview.adapter = adapter
