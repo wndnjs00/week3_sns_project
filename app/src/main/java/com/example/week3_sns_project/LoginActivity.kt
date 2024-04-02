3243c78885cce431891c2a18bfa8282e1aebe47d
SEYOUNG PARK <seyeongb18@gmail.com>
2024-04-02 20:30:02 +0900

    [FIX] 로그인, 회원가입 부분 인텐트 넘어가는 코틀린 코드 구현

--- app/src/main/java/com/example/week3_sns_project/LoginActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/LoginActivity.kt
@@ -3,31 +3,77 @@ package com.example.week3_sns_project
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginEmailEdittext: EditText
+    private lateinit var loginPasswordEdittext: EditText
+    private lateinit var loginNameEdittext: EditText
+
+    companion object {
+        // 정보가 누적될 수 있도록 companion object로 설정
+        private var emailList = mutableListOf<String>()
+        private var passwordList = mutableListOf<String>()
+        private var nameList = mutableListOf<String>()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // 이게 무슨 말이지? (팀원설명요청)
         setContentView(R.layout.activity_login)
 
         val loginBackButton: ImageButton = findViewById(R.id.login_back_button)
         val loginButton : Button = findViewById(R.id.login_login_button)
+        loginEmailEdittext = findViewById(R.id.login_email_edittext)
+        loginPasswordEdittext = findViewById(R.id.login_password_edittext)
+        loginNameEdittext = findViewById(R.id.login_name_edittext)
+
+        val emailData = intent.getStringExtra("email")
+        emailList.add(emailData ?: "-1")
+
+        val passwordData = intent.getStringExtra("password")
+        passwordList.add(passwordData ?: "-1")
+
+        val nameData = intent.getStringExtra("name")
+        nameList.add(nameData ?: "-1")
 
         loginBackButton.setOnClickListener{
             finish()
         }
 
         loginButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if(loginEmailEdittext.length()==0||loginPasswordEdittext.length()==0||loginNameEdittext.length()==0) {
+                Toast.makeText(this, "입력하지 않은 정보가 존재합니다!",Toast.LENGTH_SHORT).show()
+            } else if(emailList.contains(loginEmailEdittext.toString())&& passwordList.contains(loginPasswordEdittext.toString())&&nameList.contains(loginNameEdittext.toString())) {
+                // 회원가입 정보에 들어있는 데이터인 경우
+                // 부정문으로 하면 코드 순서상 더 좋을텐데 방법을 모르겠음
+                // .text.toString() 이랑 .toString()이랑 차이 몰라서 코드 짧은거 사용해봄
+                // 순서까지 매치하는 코드 짜기에는 아직 힘들 것 같아서 임시로 contain만 사용함
+                val intent = Intent(this, MypageFragment::class.java)
+                intent.putExtra("email",loginEmailEdittext.text.toString())
+                intent.putExtra("name",loginNameEdittext.text.toString())
+
+                val intent2 = Intent(this, MainActivity::class.java)
+                startActivity(intent2)
+                /*
+                intent 를 두개 설정한 이유 → intent 한개로 데이터 받고 MainActivity로 가면
+                MainActivity 에서 또 MypageFragment로 데이터 전달해야 하니까
+                */
+            } else {
+                // 회원가입 정보에서 받은 데이터가 아닌 경우
+                Toast.makeText(this,"유효하지 않은 정보입니다!",Toast.LENGTH_SHORT).show()
+            }
+
         }
 
-
+        // 코드 설명 리뷰 요청
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/week3_sns_project/SignUpActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/SignUpActivity.kt
@@ -1,24 +1,54 @@
 package com.example.week3_sns_project
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var signupEmailEdittext: EditText
+    private lateinit var signupPasswordEdittext: EditText
+    private lateinit var signupCheckpasswordEdittext: EditText
+    private lateinit var signupNameEdittext: EditText
+    private lateinit var signupSignupButton: Button
+    private lateinit var singupBackButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
 
-        val singupBackButton: ImageButton = findViewById(R.id.signup_back_button)
+        singupBackButton = findViewById(R.id.signup_back_button)
+        signupSignupButton = findViewById(R.id.signup_signup_button)
 
-        singupBackButton.setOnClickListener{
-            finish()
+        signupEmailEdittext = findViewById(R.id.signup_email_edittext)
+        signupPasswordEdittext = findViewById(R.id.signup_password_edittext)
+        signupCheckpasswordEdittext = findViewById(R.id.signup_checkpassword_edittext)
+        signupNameEdittext = findViewById(R.id.signup_name_edittext)
+
+        singupBackButton.setOnClickListener {
+            if (signupEmailEdittext.length() == 0 || signupPasswordEdittext.length() == 0 || signupCheckpasswordEdittext.length() == 0 || signupNameEdittext.length() == 0) {
+                // 정보를 다 입력하지 않은 경우
+                Toast.makeText(this, "정보를 아직 다 입력하지 않았습니다", Toast.LENGTH_SHORT).show()
+            } else if (signupPasswordEdittext != signupCheckpasswordEdittext) {
+                // '비밀번호'와 '비밀번호 확인' 데이터가 다른 경우
+                Toast.makeText(this, "비밀번호가 서로 다릅니다!", Toast.LENGTH_SHORT).show()
+            } else {
+                // registerForActivityResult 을 사용할 수 있을까? 하다가 일단 intent 로 받아옴
+                intent = Intent(this, LoginActivity::class.java) // 넘기는 쪽이 FirstActivity 일 수는 없나?(팀원 리뷰 요청)
+                intent.putExtra("email",signupEmailEdittext.text.toString()) // 근데 text.toString() 말고 toString() 만 쓰는거랑 무슨 차이지?(팀원 리뷰 요청)
+                intent.putExtra("password",signupPasswordEdittext.text.toString())
+                intent.putExtra("name",signupNameEdittext.text.toString())
+                finish()
+                //startActivity(intent) 꼭 써야지 데이터가 전달되는건가??
+            }
         }
-
-
     }
 }
