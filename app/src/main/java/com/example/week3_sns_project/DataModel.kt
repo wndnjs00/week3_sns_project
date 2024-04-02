85d1b6b92bcbd9fadf163435eaedd283001e91b0
jeonjuwon <juwonjeon852@gmail.com>
2024-04-02 22:39:12 +0900

    [FIX]디렉토리 파일구조 수정

---
+++ app/src/main/java/com/example/week3_sns_project/DataModel.kt
@@ -0,0 +1,8 @@
+package com.example.week3_sns_project
+
+data class DataModel (
+
+    val image : Int,             // drawable은 숫자로 인식
+    val title : String,          // 제목
+    val content : String        // 내용
+)
