8add87802ea84bef7119c270f5bd396a921cde8d
AndroidJeong <okbamtol@gmail.com>
2024-04-02 14:41:09 +0900

    main 이름 변경

--- app/src/main/java/com/example/week3_sns_project/FirstActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/FirstActivity.kt
@@ -3,12 +3,9 @@ package com.example.week3_sns_project
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,8 +14,8 @@ class MainActivity : AppCompatActivity() {
         val loginButton: Button = findViewById(R.id.main_login_button)
         val signUpButton: Button = findViewById(R.id.main_signup_button)
 
-        val loginIntent = Intent(this@MainActivity, LoginActivity::class.java)
-        val signUpIntent = Intent(this@MainActivity, SignUpActivity::class.java)
+        val loginIntent = Intent(this@FirstActivity, LoginActivity::class.java)
+        val signUpIntent = Intent(this@FirstActivity, SignUpActivity::class.java)
 
         loginButton.setOnClickListener{
             startActivity(loginIntent)
