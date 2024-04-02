37ae42454dd4ea659c6f89c061c59f5ee9c76678
jeonjuwon <juwonjeon852@gmail.com>
2024-04-02 11:23:46 +0900

    [FIX] strings 제거

--- app/src/main/java/com/example/week3_sns_project/MainActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-
         BottomNavigation()
     }
 
