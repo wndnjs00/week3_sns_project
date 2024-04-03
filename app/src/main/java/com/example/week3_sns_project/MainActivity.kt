82ab974a5c1724d04ec20a3bbb9116257773eeaa
SEYOUNG PARK <seyeongb18@gmail.com>
2024-04-03 10:41:38 +0900

    [FIX] 인텐트로 데이터 넘기기

--- app/src/main/java/com/example/week3_sns_project/MainActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/MainActivity.kt
@@ -9,15 +9,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var emailData : String
+    lateinit var nameData : String
+    lateinit var intentToMyPage : Intent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
         BottomNavigation()
 
-        val emailData = intent.getStringExtra("email")
-        val nameData = intent.getStringExtra("name")
-        val intentToMyPage = Intent(this, MypageFragment::class.java)
+        emailData = intent.getStringExtra("email") ?: "-1"
+        nameData = intent.getStringExtra("name") ?: "-1"
+        intentToMyPage = Intent(this, MypageFragment::class.java)
     }
 
 
@@ -50,9 +54,9 @@ class MainActivity : AppCompatActivity() {
                 // mypage클릭시 MypageFragment()로 대체
                 R.id.mypage -> {
                     supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_framelayout, MypageFragment())
+                        .replace(R.id.main_framelayout, MypageFragment()) // fragment 를 쓸 때는 frameLayout을 꼭 써야하는가?
                         .commitAllowingStateLoss()
-
+                    // intent 못넘김 .. 데이터 어떻게 넘길지 모르겠음 (추가공부 필요)
                     return@setOnItemSelectedListener true
                 }
 
