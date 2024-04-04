ac4a0b9cf60d3b42921f0b32201a66fb1aee7237
jeonjuwon <juwonjeon852@gmail.com>
2024-04-04 10:27:23 +0900

    [FIX]데이터넣는 구조 변경

--- app/src/main/java/com/example/week3_sns_project/HomeFragment.kt
+++ app/src/main/java/com/example/week3_sns_project/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.week3_sns_project
 
 import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,13 +13,13 @@ import com.example.week3_sns_project.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding      // 뷰바인딩 선언(초기화는 나중에)
-    private lateinit var itemList : ArrayList<DataModel>    // 리스트에 DataModel을 담은 itemList선언
 
-    // StringModel에 string값 넣어주기
-    private val stringModel = StringModel(stringId = R.string.title_content_tv)
-    private val stringModel2 = StringModel(stringId = R.string.title_content_tv2)
-    private val stringModel3 = StringModel(stringId = R.string.post_tv1)
-    private val stringModel4 = StringModel(stringId = R.string.post_tv2)
+
+
+
+
+
+
 
 
     override fun onCreateView(
@@ -28,14 +29,14 @@ class HomeFragment : Fragment() {
         // 뷰바인딩 초기화
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        // itemList 초기화
-        itemList = ArrayList()
 
-        // itemList에 image,title,content,nickname에 해당하는 데이터 추가
-        // title,content에는 string값을 넣어줌
-        itemList.add(DataModel(R.drawable.img_bts, stringModel.toString(requireContext()), stringModel3.toString(requireContext()), "BTS", R.drawable.img_bts))
-        itemList.add(DataModel(R.drawable.img_btob, stringModel2.toString(requireContext()), stringModel4.toString(requireContext()), "BTOB", R.drawable.img_btob))
 
+
+
+
+
+
+        val itemList = HomeList(resources)
 
         val adapter = HomeAdapter(itemList)             // HomeAdapter에 itemList데이터 담기
         binding.homeRecyclerview.adapter = adapter      // 리사이클러뷰 레이아웃과 어뎁터 연결 (어뎁터가 중간다리 역할이기 때문에 Fragment와 Adapter를 연결시켜줘야함!!)
@@ -57,5 +58,27 @@ class HomeFragment : Fragment() {
 
 
         return binding.root
+    }
+
+
+    fun HomeList(resources: Resources) : ArrayList<DataModel>{
+
+        return arrayListOf(
+            DataModel(
+                image = R.drawable.img_bts,
+                title = resources.getString(R.string.title_content_tv),
+                content = resources.getString(R.string.post_tv1),
+                nickName = resources.getString(R.string.bts),
+                profile_image = R.drawable.img_bts
+            ),
+            DataModel(
+                image = R.drawable.img_btob,
+                title = resources.getString(R.string.title_content_tv2),
+                content = resources.getString(R.string.post_tv2),
+                nickName = resources.getString(R.string.btob),
+                profile_image = R.drawable.img_btob
+            )
+        )
+
     }
 }
