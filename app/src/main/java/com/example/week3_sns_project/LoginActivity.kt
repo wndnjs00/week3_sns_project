493716c5ae8257b20004cb7c9fc56b088b7b03bb
AndroidJeong <okbamtol@gmail.com>
2024-04-04 09:57:56 +0900

    [MERGE]

--- app/src/main/java/com/example/week3_sns_project/LoginActivity.kt
+++ app/src/main/java/com/example/week3_sns_project/LoginActivity.kt
@@ -78,7 +78,7 @@ class LoginActivity : AppCompatActivity() {
 
         }
 
-        // 코드 설명 리뷰 요청
+        // 코드 설명 리뷰 요청.
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
