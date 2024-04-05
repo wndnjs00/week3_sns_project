a86c732fdf7d8d70658d63578c880c7e7a4e1507
jeonjuwon <juwonjeon852@gmail.com>
2024-04-05 10:06:47 +0900

    [FEAT]마이페이지에 스피너 추가

--- app/src/main/java/com/example/week3_sns_project/MypageFragment.kt
+++ app/src/main/java/com/example/week3_sns_project/MypageFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.week3_sns_project.databinding.FragmentMypageBinding
 
 class MypageFragment : Fragment() {
@@ -25,6 +27,40 @@ class MypageFragment : Fragment() {
         binding.mypageIdTextview2?.text = id
         binding.mypageNameTextview2?.text = name
 
+        spinnerIdol()
+        spinnerIdolResult()
+
         return binding.root
     }
+
+
+    // spinner의 값을 결정하는 함수
+    fun spinnerIdol(){
+
+        val idol = resources.getStringArray(R.array.itemList)
+
+        val Adapter = context?.let { ArrayAdapter(it, R.layout.item_spinner, idol) }
+
+        binding.mypageSpinner?.adapter = Adapter
+    }
+
+
+    fun spinnerIdolResult(){
+
+        binding.mypageSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                binding.mypageArtistTextview2.text = "${binding.mypageSpinner?.getItemAtPosition(position)}"
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+    }
+
 }
