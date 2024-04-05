0d41ce72407fd1ab6499e74768d3f51bebf87fb9
jeonjuwon <juwonjeon852@gmail.com>
2024-04-05 19:32:49 +0900

    [FIX]내용값 string변경

--- app/src/main/java/com/example/week3_sns_project/HomeFragment.kt
+++ app/src/main/java/com/example/week3_sns_project/HomeFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.week3_sns_project.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
